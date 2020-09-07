55b4fc157cd1b3b86cb2fb07729bebc9f061ed47
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-07 17:18:25 +0300

    Edited Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Login, Register, Feed } from './Routes/index';
+import NotFound from './error404';
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
           <Route exact path='/' component={Feed} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
+
+          {/* Error 404 - Not found */}
+          <Route path='*' component={NotFound} />
         </Switch>
       </Router>
     </>
