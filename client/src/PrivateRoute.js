4d54adef563aba716db625482938f9d84af1126e
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-09 14:21:11 +0300

    Private Route

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, Auth, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        localStorage.getItem('token');
+        if (localStorage.token) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to='/login' />;
+        }
+      }}
+    />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  Auth: state.Auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
