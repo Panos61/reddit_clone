aa22d1a6ab76f60c7eff6a8a0a70c0f1b9ba850f
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-10 17:04:28 +0300

    Added guest buttons

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import {
   Collapse,
+  Button,
   Navbar,
   NavbarToggler,
   NavbarBrand,
@@ -15,8 +16,65 @@ import {
 } from 'reactstrap';
 import logo from '../img/reddit_logo.png';
 import './Header.css';
+import Logout from './Logout';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const Header = (props) => {
+const authMenu = (
+  <Fragment>
+    <Logout />
+  </Fragment>
+);
+
+const guestMenu = (
+  <Fragment>
+    <Link to='/login'>
+      <button className='guest-btn-login'>LOG IN</button>
+    </Link>
+
+    <Link to='/register'>
+      <button className='guest-btn-register'>SIGN UP</button>
+    </Link>
+
+    <UncontrolledDropdown>
+      <DropdownToggle nav caret style={{ color: '#1c1c2a', fontSize: '15px' }}>
+        <i
+          className='fa fa-user'
+          aria-hidden='true'
+          style={{ marginRight: '10px', color: '#878a8c', fontSize: '17px' }}
+        />
+      </DropdownToggle>
+      <DropdownMenu right>
+        <DropdownItem header style={{ fontSize: '11px', fontWeight: 'bold' }}>
+          VIEW OPTIONS
+        </DropdownItem>
+
+        <DropdownItem>
+          <i
+            className='fa fa-moon-o'
+            aria-hidden='true'
+            style={{ marginRight: '8px' }}
+          ></i>
+          Night Mode
+        </DropdownItem>
+        <DropdownItem divider></DropdownItem>
+        <DropdownItem>
+          <i
+            className='fa fa-sign-out'
+            aria-hidden='true'
+            style={{ marginRight: '8px' }}
+          ></i>
+          Log In / Sign Up
+        </DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  </Fragment>
+);
+
+const Header = () => {
+  const currentState = useSelector((state) => state);
+  const { isAuthenticated } = currentState.Auth;
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
@@ -24,7 +82,7 @@ const Header = (props) => {
   return (
     <div>
       <Navbar color='light' light expand='md'>
-        <NavbarBrand href='/'>
+        <NavbarBrand href='/' style={{ fontWeight: '500' }}>
           <img src={logo} alt='' />
           reddit
         </NavbarBrand>
@@ -43,89 +101,47 @@ const Header = (props) => {
               </DropdownMenu>
             </UncontrolledDropdown>
 
-            <NavItem className='center'>
+            <NavItem>
               <Input
-                className='nav-search'
-                type='email'
-                name='email'
-                id='exampleEmail2'
-                placeholder='default'
+                className='nav-search center'
+                type='text'
+                name='search'
+                placeholder='Search'
               />
             </NavItem>
           </Nav>
           <Nav>
             <NavbarText>
               <div className='header__middle'>
-                <div className='header__option'>
-                  <i class='fa fa-line-chart' aria-hidden='true'></i>
-                </div>
-
-                <div className='header__option'>
-                  <i class='fa fa-bar-chart' aria-hidden='true'></i>
-                </div>
-                <div className='header__option'>
-                  <i class='fa fa-commenting-o' aria-hidden='true'></i>
-                </div>
-                <div className='header__option'>
-                  <i class='fa fa-envelope' aria-hidden='true'></i>
-                </div>
-                <div className='header__option'>
-                  <i class='fa fa-pencil-square-o' aria-hidden='true'></i>
-                </div>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                  <div className='header__option'>
+                    <i className='fa fa-line-chart' aria-hidden='true'></i>
+                  </div>
+                </Link>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                  <div className='header__option'>
+                    <i className='fa fa-bar-chart' aria-hidden='true' />
+                  </div>
+                </Link>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                  <div className='header__option'>
+                    <i className='fa fa-commenting-o' aria-hidden='true' />
+                  </div>
+                </Link>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                  <div className='header__option'>
+                    <i className='fa fa-envelope' aria-hidden='true' />
+                  </div>
+                </Link>
+                <Link to='/submit' style={{ textDecoration: 'none' }}>
+                  <div className='header__option'>
+                    <i className='fa fa-pencil-square-o' aria-hidden='true' />
+                  </div>
+                </Link>
               </div>
             </NavbarText>
           </Nav>
-          <UncontrolledDropdown>
-            <DropdownToggle
-              nav
-              caret
-              style={{ color: '#1c1c2a', fontSize: '15px' }}
-            >
-              Meddle61
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>
-                <i
-                  class='fa fa-user'
-                  aria-hidden='true'
-                  style={{ marginRight: '8px' }}
-                ></i>
-                My Profile
-              </DropdownItem>
-              <DropdownItem>
-                <i
-                  class='fa fa-cog'
-                  aria-hidden='true'
-                  style={{ marginRight: '8px' }}
-                ></i>
-                Settings
-              </DropdownItem>
-              <DropdownItem
-                header
-                style={{ fontSize: '11px', fontWeight: 'bold' }}
-              >
-                VIEW OPTIONS
-              </DropdownItem>
-
-              <DropdownItem>
-                <i
-                  class='fa fa-moon-o'
-                  aria-hidden='true'
-                  style={{ marginRight: '8px' }}
-                ></i>
-                Night Mode
-              </DropdownItem>
-              <DropdownItem divider></DropdownItem>
-              <DropdownItem>
-                <i
-                  class='fa fa-sign-out'
-                  aria-hidden='true'
-                  style={{ marginRight: '8px' }}
-                ></i>
-                Log Out
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
+          {isAuthenticated ? authMenu : guestMenu}
         </Collapse>
       </Navbar>
     </div>
