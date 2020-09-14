e2f51778372eeea9cde4f9e74a4ec5c436e6ca84
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-15 00:12:00 +0300

    not found  +img

--- client/src/error404.js
+++ client/src/error404.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import notfound from './img/not-found.png';
 
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', position: 'relative', top: '40vh' }}>
-      <h2>Oops! Error 404 - Not found</h2>
-      <Link to='/'>Front Page</Link>
-    </div>
+    <>
+      <section id='login-background' style={{ height: '100vh' }}>
+        <div style={{ textAlign: 'center', position: 'relative', top: '12vh' }}>
+          <img src={notfound} alt='not-found' style={{ height: '50vh' }} />
+          <h2>Oops! Error 404 - Not found</h2>
+          <Link to='/' style={{ textAlign: 'center' }}>
+            Front Page
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
