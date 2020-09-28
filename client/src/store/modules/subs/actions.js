e21945941c00b1d6d2a02b8cb74acaecb6d35f4f
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-28 16:05:13 +0300

    Create Subreddit - Fixed Auth Error

--- client/src/store/modules/subs/actions.js
+++ client/src/store/modules/subs/actions.js
@@ -3,39 +3,30 @@ import { CREATE_SUBREDDIT_SUCCESS, CREATE_SUBREDDIT_ERROR } from './types';
 import history from '../../../history';
 
 export const createSub = ({ name, topic, description }) => {
-  const body = JSON.stringify({ name, topic, description });
-
   return async (dispatch, setAuth) => {
     try {
+      const body = { name, topic, description };
+
       const response = await fetch(
-        'https://localhost:4000/api/v1/subreddits/create',
+        'http://localhost:4000/api/v1/subreddits/create',
         {
           method: 'POST',
-          credentials: 'include',
           headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             Accept: 'application/json',
+            token: localStorage.token,
           },
-          body: JSON.stringify({
-            name: name,
-            topic: topic,
-            description: description,
-          }),
-          mode: 'no-cors',
+          body: JSON.stringify(body),
         }
       );
-
-      console.log(body);
 
       const parseRes = await response.json();
 
       if (parseRes.token) {
-        localStorage.setItem('token', parseRes.token);
+        localStorage.getItem('token', parseRes.token);
         setAuth(true);
       }
-
-      console.log(parseRes);
 
       dispatch({ type: CREATE_SUBREDDIT_SUCCESS, payload: parseRes });
       dispatch(clearErrors());
