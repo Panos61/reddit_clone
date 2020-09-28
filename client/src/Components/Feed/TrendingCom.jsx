db22e41ee2768ce7e3535650810f7763e0826539
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-28 18:04:41 +0300

    Trending Com Card

--- client/src/Components/Feed/TrendingCom.jsx
+++ client/src/Components/Feed/TrendingCom.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Trending.css';
 import { Card, CardTitle, CardBody } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSubreddits } from '../../store/modules/subs/actions';
+import TrendingComLink from './TrendingComLink';
 
 const TrendingCom = () => {
+  const dispatch = useDispatch();
+
+  const subredditSelector = useSelector((state) => state.SubReddit);
+  const getaListOfSubs = () => dispatch(getSubreddits());
+
+  useEffect(() => {
+    getaListOfSubs();
+  }, []);
+
+  const subreddits = subredditSelector.subreddits.map((subreddit) => {
+    return (
+      <div key={subreddit.subreddit_id}>
+        {' '}
+        <TrendingComLink
+          subreddit={subreddit}
+          key={subreddit.subreddit_id}
+        />{' '}
+      </div>
+    );
+  });
+
   return (
     <>
-      <Card body style={{ marginTop: '20%' }}>
+      <Card body style={{ marginTop: '27%' }}>
         <CardTitle>
-          <h6>Trending Communities</h6>
+          <h6 style={{ fontWeight: 'bold' }}>Trending Communities</h6>
         </CardTitle>
-        <CardBody>
-          [No data] <br />
-          [No data] <br />
-          [No data] <br />
-          [No data] <br />
-          [No data] <br />
-        </CardBody>
+        <CardBody>{subreddits}</CardBody>
       </Card>
     </>
   );
---
+++ client/src/Components/Feed/TrendingComLink.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TrendingComLink = ({ subreddit }) => {
+  return (
+    <>
+      <div className='trending-card-container'>
+        <div className='trending-card-subs'>
+          <span>
+            r/{subreddit.subreddit_name}{' '}
+            <p style={{ fontWeight: '300' }}>[..] members</p>
+          </span>
+        </div>
+        <div className='trending-card-btn'>JOIN</div>
+      </div>
+    </>
+  );
+};
+
+export default TrendingComLink;
