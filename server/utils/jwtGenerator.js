fcc5e692c2ceab9cfff9e91e0cf569bf47b29144
Panos61 <panagiwtis.orovas@gmail.com>
2020-09-28 21:45:20 +0300

    Increased jwt exp time

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -6,7 +6,7 @@ function jwtGenerator(user_id) {
     user: user_id,
   };
 
-  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: '3hr' });
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: '10d' });
 }
 
 module.exports = jwtGenerator;
